1ed922e0e1444ba4fad31bc1bf67258df61da9cc
philippschmalen <philippschmalen@gmail.com>
2024-04-26 09:59:50 +0200

    added explanation for main exec

--- src/example.main.py
+++ src/example.main.py
@@ -1,3 +1,15 @@
+"""
+Example for a main.py to setup a logger and load configuration from a .env file.
+Could be used as an entrypoint.
+
+1. Loads environment variables from the .env file using `dotenv.load_dotenv()`.
+2. Retrieves the loaded configuration using `dotenv.dotenv_values()`.
+3. Sets up a logger using `setup_logging()`.
+4. Logs a message to indicate that the module has been executed.
+
+Note: Make sure to have PYTHONPATH at project root. Otherwise imports will not work.
+"""
+
 import dotenv
 
 from conf.logging_config import setup_logging
